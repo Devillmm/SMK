3529aec5f9de3b5b3c1f872871c554b8ebd30ff4
Manikandan042 <manisuriyakumar042@gmail.com>
2026-07-23 13:25:37 +0530

    Image Resize basic Program in OpenCV

---
+++ Python_AI/AI/OpenCV/Basic_Programs/Resize_Img/Img_Resize.py
@@ -0,0 +1,26 @@
+## To Resize an Image using IMUTILS Library
+
+# Import OpenCV Library
+import cv2
+
+# Import Imutils Library
+import imutils
+
+# Read the Image from Local Directory
+Img = cv2.imread(r"C:\Users\ADMIN\Desktop\SMK\Python_AI\AI\OpenCV\Basic_Programs\Resize_Img\SMK2.png")
+
+# Show / Display the Image
+cv2.imshow("Original_Image",Img)
+
+# Resize the Original Image using Imutils
+Resize = imutils.resize(Img,width=500)
+
+# Show / Display the Resized Image
+cv2.imshow("Resized Image",Resize)
+
+# Write & Save the Resized Image
+cv2.imwrite("Basic_Programs\Resize_Img\Output\Resized.jpg",Resize)
+
+# WaitKey & Destroy All Windows
+cv2.waitKey(5000)
+cv2.destroyAllWindows()
