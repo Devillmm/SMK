875c0d25d65fb67996503a0340b8e5e7f6b7b395
Manikandan042 <manisuriyakumar042@gmail.com>
2026-07-23 13:19:44 +0530

    Grayscale Program in OpenCV

---
+++ Python_AI/AI/OpenCV/Basic_Programs/GrayScale/Grayscale.py
@@ -0,0 +1,25 @@
+## To Convert Original Image to Grayscale Image using OpenCV Library
+
+# Import OpenCV Library
+import cv2
+
+# Read the Image from Local Directory
+O_Img = cv2.imread(r"C:\Users\ADMIN\Desktop\SMK\Python_AI\AI\OpenCV\Basic_Programs\GrayScale\ironman.jpg")
+
+# Show / Display the Original Image
+cv2.imshow("Original Image",O_Img)
+
+# Convert to Grayscale Image
+grayscale = cv2.cvtColor(O_Img,cv2.COLOR_BGR2GRAY)
+
+# Show / Display the Grayscale Image
+cv2.imshow("Grayscale Image",grayscale)
+
+# Write & Save the Grayscale Image
+cv2.imwrite("Basic_Programs\GrayScale\Output\Mark42.jpeg",grayscale)
+
+# Wait Key - Key to Exit from Output
+cv2.waitKey(10000)
+
+# Destroy All Windows
+cv2.destroyAllWindows()
